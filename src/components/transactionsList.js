a9b873a23c9940b1c0087daa8b108aee3634939e
Henry Ho <henryho@Henrys-MacBook-Pro.local>
2019-01-10 15:34:35 -0800

    - Functional button dock for pagination

--- src/components/transactionsList.js
+++ src/components/transactionsList.js
@@ -1,7 +1,33 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import styled from 'styled-components'
 import TransactionCard from './transactionCard'
 import StyledUL from './styled/StyledUnorderedList'
+
+const NavigationButtonDock = styled.div`
+    display: flex;
+    justify-content: center;
+    margin: 5px;
+
+    span {
+        width: 200px;
+        text-align: center;
+    }
+`;
+
+const NavigationButton = styled.button`
+    border: none;
+    text-decoration: underline;
+
+    :hover {
+        text-decoration: none;
+        color: #82204A;
+    }
+
+    :focus {
+        outline: 0;
+    }
+`;
 
 class TransactionsList extends Component {
     constructor(props) {
@@ -33,13 +59,22 @@ class TransactionsList extends Component {
         }
     }
 
+    adjustEndIndex = () => {
+        if(this.state.endIndex > this.props.transactions.length) {
+            this.setState({ endIndex: this.props.transactions.length })
+        } 
+    }
+
     render() {
         if (this.props.transactions) {
+            this.adjustEndIndex()
             return (
                 <div className="TransactionsList">
-                    <div>
-                        <button onClick={this.prevPage}>Back</button>Showing {this.state.startIndex + 1} - {this.state.endIndex} of {this.props.transactions.length}<button onClick={this.nextPage}>Next</button>
-                    </div>
+                    <NavigationButtonDock>
+                        <NavigationButton onClick={this.prevPage} active={!(this.state.startIndex === 0)}>Back</NavigationButton>
+                        <span>Showing {this.state.startIndex + 1} - {this.state.endIndex} of {this.props.transactions.length}</span>
+                        <NavigationButton onClick={this.nextPage} active={!(this.state.endIndex === this.props.transactions.length)}>Next</NavigationButton>
+                    </NavigationButtonDock>
                     <span>Number of transactions: {this.props.transactions.length}</span>
                     <StyledUL>
                         {this.props.transactions.slice(this.state.startIndex, this.state.endIndex).map(function (transaction, index) {
@@ -50,6 +85,11 @@ class TransactionsList extends Component {
                             )
                         })}
                     </StyledUL>
+                    <NavigationButtonDock>
+                        <NavigationButton onClick={this.prevPage} active={!(this.state.startIndex === 0)}>Back</NavigationButton>
+                        <span>Showing {this.state.startIndex + 1} - {this.state.endIndex} of {this.props.transactions.length}</span>
+                        <NavigationButton onClick={this.nextPage} active={!(this.state.endIndex === this.props.transactions.length)}>Next</NavigationButton>
+                    </NavigationButtonDock>
                 </div>
             )
         } else {
@@ -57,26 +97,6 @@ class TransactionsList extends Component {
         }
     }
 }
-// const TransactionsList = ({ transactions }) => {
-//     if (transactions) {
-//         return (
-//             <div className="TransactionsList">
-//                 <span>Number of transactions: { transactions.length }</span>
-//                 <StyledUL>
-//                     {transactions.map(function (transaction, index) {
-//                         return (
-//                             <li key={transaction.transactionId}>
-//                                 <TransactionCard info={transaction} />
-//                             </li>
-//                         )
-//                     })}
-//                 </StyledUL>
-//             </div>
-//         )
-//     } else {
-//         return (<span>Loading...</span>)
-//     }
-// }
 
 TransactionsList.propTypes = {
     transactions: PropTypes.arrayOf(
